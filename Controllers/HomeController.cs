0eb4c54eec0763d4a9861151f10abdd910bacae1
efortepc12 <jawwadahsance08@gmail.com>
2020-07-06 04:37:42 -0700

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoogleDriveUploadMVC.Models;
+
+namespace GoogleDriveUploadMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            GoogleDriveAPIHelper.UplaodFileOnDrive(file);
+            ViewBag.Success = "File Uploaded on Google Drive";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
